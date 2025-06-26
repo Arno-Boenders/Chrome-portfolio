6d28038ea3db180f7dd9a77d023c0311dd49daac
Arno-Boenders <arnoboenders@gmail.com>
2025-06-26 11:29:27 +0200

    ✨ added themecontext

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { createContext, useContext, useState, useEffect } from 'react';
+
+
+const lightTheme = {
+    background: 'bg-white',
+    foreground: 'text-gray-900',
+};
+
+const darkTheme = {
+    background: 'bg-[#202124]',
+    foreground: 'text-gray-100',
+};
+
+const ThemeContext = createContext({
+    isDarkMode: false,
+    toggleTheme: () => { },
+    theme: lightTheme, // default
+});
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleTheme = () => setIsDarkMode(!isDarkMode);
+
+    const theme = isDarkMode ? darkTheme : lightTheme;
+
+    return (
+        <ThemeContext.Provider value={{ isDarkMode, toggleTheme, theme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => useContext(ThemeContext);
